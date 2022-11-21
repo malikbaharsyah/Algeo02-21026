83ee88dd12724b25f689a6e0a75284938e641cae
Darkfirm <16521490@mahasiswa.itb.ac.id>
2022-11-21 17:02:38 +0700

    add  execution time

--- src/gui.py
+++ src/gui.py
@@ -19,6 +19,14 @@ persen_lbl = PhotoImage(file="./assets/persentase.png")
 labelpersen = Label(window, image=persen_lbl, border=0, bg='#FFF7DF')
 labelpersen.place(x=45, y=390)
 
+extime_1 = PhotoImage(file="./assets/train.png")
+labelextime1 = Label(window, image=extime_1, border=0, bg='#FFF7DF')
+labelextime1.place(x=375, y=490)
+
+extime_2 = PhotoImage(file="./assets/detect.png")
+labelextime2 = Label(window, image=extime_2, border=0, bg='#FFF7DF')
+labelextime2.place(x=600, y=490)
+
 
 img_inactive = Image.open("./assets/img1.png")
 img_active = Image.open("./assets/img2.png")
@@ -52,9 +60,31 @@ def ChooseFolder():
     filepath = DirectoryFolder()
     dataF = os.path.split(filepath)
     out=dataF[1]
+
     eigenfaces = getEigenFace.getEigenFace(filepath)
+
+    elapsed_time_training = getEigenFace.getElapsedTimeTraining()
+    time_text = str(round(elapsed_time_training, ndigits=2)) + " detik"
+    time_lbl = Label(window, text=time_text, bg='#FFF7DF', font=("Arial", 25, "bold"), fg="#76460E")
+    time_lbl.place(x=375, y=510)
+
     imglbl1=Label(window, text=out, bg='#FFF7DF')#my_string_var1.set(out))
     imglbl1.place(x=172, y=228)
+
+    popupmsg("Proses mendapatkan gambar menjadi matriks pada folder "+out+" selesai")
+
+#make a pop up message after get the filepath
+def popupmsg(msg):
+    popup = Tk()
+    popup.configure(background='#FFF7DF')
+    popup.iconbitmap(r'./assets/icon.ico')
+    popup.geometry("+%d+%d" % ((window.winfo_screenwidth() - 500) / 2, (window.winfo_screenheight() - 100) / 2))
+    popup.title("Yeay!")
+    label = Label(popup, text=msg, font=("Arial", 10), bg='#FFF7DF', fg="#76460E")
+    label.pack(side="top", fill="x", pady=10)
+    B1 = Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
 
 def ImageProccess():
     global objPic
@@ -69,21 +99,27 @@ def ImageProccess():
     data = os.path.split(objPic)
     out=data[1]
     imglbl2=Label(window, text=out, bg='#FFF7DF')#my_string_var2.set(out))
-    imglbl2.place(x=172, y=288)
-    
+
+    imglbl2.place(x=320, y=426)   
     hasil = getEigenFace.detectHasil(eigenfaces, filepath, objPic)
-    persentase = hasil[1]
+    
+    persentase = round(hasil[1], ndigits=2)
     persen_text = Label(window, text=str(persentase)+"%", bg='#FFF7DF', font=("Arial", 40, "bold"), fg="#76460E")
     persen_text.place(x=50, y=425)
 
+    elapsed_time_detection = getEigenFace.getElapsedTimeDetection()
+    time_text = str(round(elapsed_time_detection, ndigits=2)) + " detik"
+    time_lbl = Label(window, text=time_text, bg='#FFF7DF', font=("Arial", 25, "bold"), fg="#76460E")
+    time_lbl.place(x=600, y=510)
+
     image2 = Image.open(os.path.join(filepath, hasil[0]))
+    nama1=Label(window, text=hasil[0], bg='#FFF7DF', font=("Arial", 11, "bold"), fg="#76460E")
+    nama1.place(x=620, y=426)
     image2 = image2.resize((256, 256), Image.ANTIALIAS)
     image2X = ImageTk.PhotoImage(image2)
     label2 = Label(window, image=image2X, )
     label2.image = image2X
     label2.place(x=625, y=140)
-
-    
 
 def change_cursor(event):
     window.config(cursor="heart")
@@ -123,7 +159,6 @@ button2 = Button(window, image=window.folder_inactive, border=0, bg='#FFF7DF', a
 button2.place(x=40, y=220)
 button3 = Button(window, image=window.result_inactive, border=0, bg='#FFF7DF', activebackground='#FFF7DF')
 button3.place(x=45, y=348)
-
 tipek_lbl = PhotoImage(file="./assets/typex.png")
 labeltipek = Label(window, image=tipek_lbl, border=0, bg='#FFF7DF')
 labeltipek.place(x=45, y=348)
